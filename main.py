c200ae3f4acebded3929fd3b7f29dc0f74dc1205
iagyinjoe <32269540-iagyinjoe@users.noreply.replit.com>
2024-02-23 12:41:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Getting to know you!')
+print()
+
+name = input("What's your name?: ")
+food = input("What's your favourite food?: ")
+music = input("What's your favourite music?: ")
+resident = input("Where do you live?: ")
+
+print()
+print("You are")
+print(name)
+print("You are probably hungry for")
+print(food)
+print("and you're definitely getting your groove on to")
+print(music)
+print("living in the amazing")
+print(resident)
+      
